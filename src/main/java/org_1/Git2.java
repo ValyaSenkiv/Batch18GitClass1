d767e68cdaec6ced2a05c37978d8f0fdc2270b2c
ValyaSenkiv <senkiv.valya@gmail.com>
2023-12-07 14:02:15 -0800

    second change

---
+++ src/main/java/org_1/Git2.java
@@ -0,0 +1,4 @@
+package org_1;
+
+public class Git2 {
+}
--- src/main/java/org_1/Git_1.java
+++ src/main/java/org_1/Git_1.java
@@ -3,5 +3,6 @@ package org_1;
 public class Git_1 {
     public static void main(String[] args) {
         System.out.println("Class 1");
+        //first change after git init
     }
 }
