f615870f2a4dd0de9733b2daaaf0dbb433f1035e
ValyaSenkiv <senkiv.valya@gmail.com>
2023-12-09 23:56:21 -0800

    Branch 1

--- src/main/java/org_1/Git_1.java
+++ src/main/java/org_1/Git_1.java
@@ -5,7 +5,6 @@ public class Git_1 {
         System.out.println("Class 1");
         //first change after git init
         //Third change
-        //change from branch number 1
-        //git change in branchNumber 1
+
     }
 }
